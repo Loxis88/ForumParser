209f8532828d8e1c9e150d7bfffb91dc94654832
Loxiss88 <grisham436@gmail.com>
2024-12-11 04:14:21 +0300

    trying fix parrallel

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -28,6 +30,62 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Инициализация коллектора и настройка
+	c := colly.NewCollector(colly.Async(true))
+
+	errLim := c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Parallelism: 20,
+		Delay:       100 * time.Millisecond,
+	})
+	if errLim != nil {
+		log.Fatal(errLim)
+		return
+	}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var results [][]string // Срез для хранения результатов
+
+	// Добавление обработчика для каждого найденного сообщения
+	c.OnHTML("div.post_wrap", func(e *colly.HTMLElement) {
+		defer wg.Done() // Уменьшаем счетчик после завершения обработки
+
+		postWrap := e.DOM
+
+		username := postWrap.Find("div.user_details span[itemprop='name']").Text()
+		message := postWrap.Find("div[itemprop='commentText']:not(blockquote.ipsBlockquote)").Text()
+
+		mu.Lock()
+		results = append(results, []string{strings.TrimSpace(username), strings.TrimSpace(message)})
+		mu.Unlock()
+	})
+
+	c.OnHTML("div.topic_controls:not(.ipsPad_top_bottom_half) a[rel='next']", func(e *colly.HTMLElement) {
+		nextPage := e.Attr("href")
+		if nextPage != "" {
+			wg.Add(1) // Увеличиваем счетчик перед вызовом Visit
+			err := e.Request.Visit(nextPage)
+			if err != nil {
+				log.Fatalf("Error visiting next page: %v", err)
+			}
+		}
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	for _, link := range links {
+		wg.Add(1) // Увеличиваем счетчик перед вызовом Visit
+		err := c.Visit(link)
+		if err != nil {
+			log.Printf("Error visiting %s: %v\n", link, err)
+		}
+	}
+
+	c.Wait() // Ожидание завершения всех асинхронных запросов
+
 	// Открытие файла для записи результатов
 	outputFile, err := os.Create("output.csv")
 	if err != nil {
@@ -36,56 +94,23 @@ func main() {
 	defer outputFile.Close()
 
 	writer := csv.NewWriter(outputFile)
-	writer.Comma = '\t' //  разделитель
+	writer.Comma = '\t' // разделитель
 	defer writer.Flush()
 
 	// Запись заголовков в CSV
 	errWr := writer.Write([]string{"username", "message"})
-	if err != nil {
+	if errWr != nil {
 		log.Fatalf("Error writing record to csv: %v", errWr)
 		return
 	}
 
-	// Инициализация коллектора и настройка
-	c := colly.NewCollector()
-
-	// Добавление обработчика для каждого найденного сообщения
-	c.OnHTML("div.post_wrap", func(e *colly.HTMLElement) {
-		postWrap := e.DOM
-		username := postWrap.Find("div.user_details span[itemprop='name']").Text()
-		message := postWrap.Find("div[itemprop='commentText']").Text()
-
-		fmt.Printf("author: %s\nmessage: %s\n", strings.TrimSpace(username), strings.TrimSpace(message))
-		err := writer.Write([]string{strings.TrimSpace(username), strings.TrimSpace(message)})
-		if err != nil {
-			log.Fatalf("Error writing record to csv: %v", errWr)
-			return
-		}
-	})
-
-	c.OnHTML("div.topic_controls:not(.ipsPad_top_bottom_half) a[rel='next']", func(e *colly.HTMLElement) {
-		nextPage := e.Attr("href")
-		if nextPage != "" {
-			fmt.Println("visiting next page: ", nextPage)
-			err := e.Request.Visit(nextPage)
-			if err != nil {
-				log.Fatalf("Error visiting next page: %v", err)
-				return
-			}
-		}
-	})
-
-	// Обработчик ошибок
-	c.OnError(func(r *colly.Response, err error) {
-		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-	})
-
-	// Обработка каждой ссылки
-	for _, link := range links {
-		err := c.Visit(link)
-		fmt.Println("Visiting URL:", link)
-		if err != nil {
-			log.Printf("Error visiting %s: %v\n", link, err)
+	// Запись результатов в CSV
+	for _, result := range results {
+		errWr := writer.Write(result)
+		if errWr != nil {
+			log.Fatalf("Error writing record to csv: %v ", errWr)
 		}
 	}
+
+	fmt.Println("\nResults have been written to output.csv")
 }
